4302c777c389c8944a49529dbd6036081b2f68db
amanvir uppal <amanvir.uppal@icloud.com>
2019-10-15 13:32:39 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,4 +1,9 @@
 def bonus
+  
+  epic_tragedy[:montague][:hero][:status]="dead"
+  epic_tragedy[:capulet][:heroine][:status]="dead"
+  
+  puts epic_tragedy
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
